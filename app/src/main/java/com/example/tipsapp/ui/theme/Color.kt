b73cb5be20a17dbb0442229aef05d4859670b4cd
jaweed3 <wedjaw22@gmail.com>
2026-06-09 18:18:07 +0700

    feat: kustomisasi tema warna dengan nuansa hijau restoran

---
+++ app/src/main/java/com/example/tipsapp/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.tipsapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Green80 = Color(0xFFA5D6A7)
+val Teal80 = Color(0xFF80CBC4)
+val Orange80 = Color(0xFFFFCC80)
+
+val Green40 = Color(0xFF388E3C)
+val Teal40 = Color(0xFF00897B)
+val Orange40 = Color(0xFFF57C00)
